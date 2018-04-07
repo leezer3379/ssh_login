6ec2f91935ab407ab9d1da26adc1047dacb36bcf
leezer3379 <leezer3379@163.com>
2018-04-07 10:33:32 +0800

    update

---
+++ my_paramiko.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18-4-7 上午9:02
+# @Author  : Leezer3379
+# @Site    : https://github.com/leezer3379
+# @File    : my_paramiko.py
+# @Software: PyCharm
+import paramiko
+host = '10.211.55.13'
+user = 'root'
+port = 22
+passwd = 'huan@123'
+## key
+# pkssh = paramiko.SSHClient()
+# pkssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#
+# key = paramiko.RSAKey.from_private_key_file('/root/.ssh/id_rsa')
+# pkssh.connect(hostname=host, port=port, username=user, pkey=key)
+#
+# while True:
+#     if user == 'root':
+#         stdin, stdout, stderr = pkssh.exec_command('pwd')
+#         cmd = raw_input('root@gavin-virtual-machine:%s#' % stdout.read().strip())
+#     else:
+#         stdin, stdout, stderr = pkssh.exec_command('pwd')
+#         cmd = raw_input('%s@gavin-virtual-machine:%s$' % (user,stdout.read().strip()))
+#     if cmd == 'exit':
+#         pkssh.close()
+#         exit()
+#     stdin, stdout, stderr = pkssh.exec_command(cmd)
+#
+#     if not stdout:
+#         print(stderr.read().strip())
+#
+#     print(stdout.read().strip())
+
+## pass
+pkssh = paramiko.SSHClient()
+pkssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+pkssh.connect(hostname=host, port=port, username=user, password=passwd)
+
+while True:
+    if user == 'root':
+        stdin, stdout, stderr = pkssh.exec_command('pwd')
+        cmd = raw_input('root@gavin-virtual-machine:%s#' % stdout.read().strip())
+    else:
+        stdin, stdout, stderr = pkssh.exec_command('pwd')
+        cmd = raw_input('%s@gavin-virtual-machine:%s$' % (user,stdout.read().strip()))
+    if cmd == 'exit':
+        pkssh.close()
+        exit()
+    stdin, stdout, stderr = pkssh.exec_command(cmd)
+
+    if not stdout:
+        print(stderr.read().strip())
+
+    print(stdout.read().strip())
